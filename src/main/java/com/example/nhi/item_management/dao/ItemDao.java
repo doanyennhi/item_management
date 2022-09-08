62c7e24da6a6f23052fa0fc913637b0ecec928f2
doanyennhi <s3880599@rmit.edu.vn>
2022-09-09 01:55:31 +1000

    Add update and delete item methods for DAO

--- src/main/java/com/example/nhi/item_management/dao/ItemDao.java
+++ src/main/java/com/example/nhi/item_management/dao/ItemDao.java
@@ -12,7 +12,7 @@ public class ItemDao {
     static {
         items.getItemList().add(new Item("1", "yeonjun", "lonely boy", 13.90));
         items.getItemList().add(new Item("2", "beomgyu", "run away", 13.30));
-        items.getItemList().add(new Item("1", "junho", "chasing tails", 30.12));
+        items.getItemList().add(new Item("3", "junho", "chasing tails", 30.12));
     }
 
     public Items getItems() {
@@ -21,5 +21,21 @@ public class ItemDao {
 
     public void addItem(Item item) {
         items.getItemList().add(item);
+    }
+
+    public void updateItem(Item item) throws Exception {
+       for (Item currentItem: items.getItemList()) {
+           if (item.getId().equals(currentItem.getId())) {
+               items.getItemList().remove(currentItem);
+               items.getItemList().add(item);
+           }
+           else {
+               throw new Exception("Item does not exist.");
+           }
+       }
+    }
+
+    public void deleteItem(Item item) {
+        items.getItemList().remove(item);
     }
 }
